c0ae8bba20ec4adb2decd3acc2c200587c890265
kalpitborkar <81480133+kalpitborkar@users.noreply.github.com>
2022-06-13 15:20:45 +0530

    Add user routes

---
+++ routes/userRoutes.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const router = express.Router();
+
+const { authenticateUser, authorizePermissions } = require('../middleware/authentication');
+
+const {
+    getAllUsers,
+    getSingleUser,
+    showCurrentUser,
+    updateUser,
+    updateUserPassword
+} = require('../controllers/userController');
+const { get } = require('mongoose');
+
+router.route('/')
+    .get([authenticateUser, authorizePermissions], getAllUsers);
+
+router.route('/showMe')
+    .get(authenticateUser, showCurrentUser);
+
+router.route('/updateUser')
+    .patch(authenticateUser, updateUser);
+
+router.route('/updateUserPassword')
+    .patch(authenticateUser, updateUserPassword);
+
+router.route('/:id')
+    .get(authenticateUser, getSingleUser);
+
+module.exports = router;
+
