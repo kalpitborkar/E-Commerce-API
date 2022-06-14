8d042a9d649a4c03094d68ca051f30b75f0aa839
kalpitborkar <81480133+kalpitborkar@users.noreply.github.com>
2022-06-14 23:22:54 +0530

    Add routes in app.js

--- app.js
+++ app.js
@@ -8,3 +8,10 @@ const app = express();
 
 // database
 const connectDB = require('./db/connect');
+
+// routers
+const authRouter = require('./routes/authRoutes');
+const userRouter = require('./routes/userRoutes');
+const productRouter = require('./routes/productRoutes');
+const reviewRouter = require('./routes/reviewRoutes');
+const orderRouter = require('./routes/orderRoutes');
